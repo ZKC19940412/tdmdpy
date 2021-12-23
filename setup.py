d38301c55f163aefce7af923234f921482ba4ee1
ZKC19940412 <zkuchen@ucdavis.edu>
2021-12-22 18:02:23 -0800

    Enable download from packages.

--- setup.py
+++ setup.py
@@ -46,6 +46,10 @@ setup(
     # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
 
+        # Additional entries you may want simply uncomment the lines you want and fill in the data
+    # url='http://www.my_package.com',  # Website
+    install_requires=requirements,              # Required packages, pulls from pip if needed; do not use for Conda deployment
+
     # Additional entries you may want simply uncomment the lines you want and fill in the data
     # url='http://www.my_package.com',  # Website
     # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
