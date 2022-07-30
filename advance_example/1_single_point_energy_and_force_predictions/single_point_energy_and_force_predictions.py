af1b1bcf00b5395ec48afd0557a1815f3575aa4f
zkunchen <zkuchen@ucdavis.edu>
2022-07-30 08:06:40 -0700

    Create single_point_energy_and_force_predictions.py

---
+++ advance_example/1_single_point_energy_and_force_predictions/single_point_energy_and_force_predictions.py
@@ -0,0 +1,32 @@
+from ase.calculators.lammpslib import LAMMPSlib
+from ase.io import read
+from pynep.calculate import NEP
+from tdmdpy.mlps import single_point_energy_force_prediction
+import sys
+
+if __name__ == "__main__":
+
+    # Uptake user define data path
+    data_path = sys.argv[1]
+
+    # Derive number of configurations
+    number_of_configurations = len(read(data_path, index = ':'))
+
+    # Define NEP calculator
+    nep_calculator = NEP('nep.txt')
+
+    # Define SNAP calculator under LAMMPSlib
+    lammps_inputs = {
+       'lmpcmds': [
+           'pair_style snap',
+           'pair_coeff * * H2O_pot.snapcoeff H2O_pot.snapparam H O'],
+       'keep_alive': True}
+    snap_calculator = LAMMPSlib(**lammps_inputs)
+    print('ML Potential : NEP')
+    single_point_energy_force_prediction(number_of_configurations, nep_calculator,
+                                         data_path, atoms_per_molecule=3)
+    
+    print('\n')
+    print('ML Potential : SNAP')
+    single_point_energy_force_prediction(number_of_configurations, snap_calculator,
+                                         data_path, atoms_per_molecule=3)
