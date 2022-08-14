467640f5c6ea98e8dcc867573bde120b3f6bc09a
zkunchen <zkuchen@ucdavis.edu>
2022-08-13 21:01:48 -0700

    Create experimetal_reference.py

---
+++ tdmdpy/experimetal_reference.py
@@ -0,0 +1,26 @@
+# Initalize a data dictionary for experimental reference, along with the force field scoring scheme proposed by Carlos Vega et al.
+# URL for the paper: 10.1039/c1cp22168j
+
+experimental_reference = {'delta_H_melt (kcal/mol)': 1.44,
+                          'delta_H_vapor (kcal/mol)': 10.52,
+                          'gamma_300K (mN/m)': 71.73,
+                          'gamma_450K (mN/m)': 42.88,
+                          'Tm (K)': 273.15,
+                          'rho_liquid @ 298.15 K, 1.01325 bar (g/cm^3)': 0.999,
+                          'rho_solid @ 273.15 K, 1.01325 bar (g/cm^3)': 0.917,
+                          'dP/dT (bar/K)': -137,
+                          'Tmax_rho (K)': 277,
+                          'rho_liquid @ 400K, 1.01325 bar (g/cm^3)': 0.9375,
+                          'kappa_T @ 298K, 1 bar (Mbar^-1)': 45.3,
+                          'Cp_liquid @ 298K, 1bar (cal/mol/K)': 18,
+                          'Cp_ice @ 250K, 1 bar (cal/mol/k)': 8.3,
+                          'rho_Ih @ 250K, 1 bar (g/cm^3)': 0.92,
+                          'rho_II @ 123K, 1 bar (g/cm^3)': 1.19,
+                          'rho_V @ 223K, 5.3 kbar (g/cm^3)': 1.283,
+                          'rho_VI @ 225K, 11 kbar (g/cm^3)': 1.373,
+                          'lnD_278K': -11.24,
+                          'lnD_298K': -10.68,
+                          'lnD_318K': -10.24,
+                          'neta @ 298K, 1 bar (mpa * s)': 0.896,
+                          'neta @ 373K 1 bar (mpa * s)': 0.284
+                          }
