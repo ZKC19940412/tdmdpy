4ebd42e4d7fc91cca41c4609f2360c8e88f26af2
zkunchen <zkuchen@ucdavis.edu>
2022-09-07 00:43:19 -0700

    Update get_pyNEP.sh

--- get_pyNEP.sh
+++ get_pyNEP.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# Pip install pyblind11 as the simlink seems to break
+pip install pybind11
+
 # Obtain PyNEP from remote
 git clone https://github.com/ZKC19940412/PyNEP.git
 
 # Install from source
 cd PyNEP
 pip install .
-
